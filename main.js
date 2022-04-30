a7ff291eb044527045d3a5b8cb22b2511727fb3f
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-04-29 19:55:04 -0500

    Agregar protoripo Bar e implementar método draw en el BoardView
    Se creó el prototipo Bar para crear las barras del juego. Su constructor recibe el alto, ancho y la posición de la barra. También se dejaron indicados los métodos de subir y bajar las barras. Por otro lado, se creó el método draw en el prototipo BoardView para dibujar los elementos en el canvas.

--- main.js
+++ main.js
@@ -13,21 +13,53 @@
         this.ball = null;
     }
 
+    /**
+     * Getter de los elementos (barras y bola)
+     */
     self.Board.prototype = {
         get elements() {
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
 })();
 
 /**
+ * Se implementa el prototipo para crear las barras
+ */
+(function () {
+    self.Bar = function (x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle"
+    }
+
+    /**
+     * Se declaran los métodos para subir y bajar las barras
+     */
+    self.Bar.prototype = {
+        down: function () {
+
+        },
+        up: function () {
+
+        }
+    }
+
+})();
+
+
+/**
  * Función anónima para crear un elemento canvas (elemento de dibujo)
  * sobre el cual mostrar el tablero
  */
-
 (function () {
+    // Protoripo para instanciar el canvas
     self.BoardView = function (canvas, board) {
         this.canvas = canvas;
         this.canvas.width = board.width;
@@ -35,19 +67,48 @@
         this.board = board;
         this.ctx = canvas.getContext("2d")
     }
+
+    /**
+     * Se añade el método para dibujar los elementos en el canvas
+     */
+    self.BoardView.prototype = {
+        draw: function () {
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i];
+                draw(this.ctx, el)
+            }
+        }
+    }
+
+    /**
+     * Función auxiliar para dibujar los elementos, no hace parte del protoripo BoardView
+     */
+    function draw(ctx, element) {
+        if (element !== null && element.hasOwnProperty("kind"))
+            switch (element.kind) {
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+    }
+
 })();
 
 /**
  * Ejecutar la función main al cargar la ventana
  * Se define el tamaño del tablero al intanciar un Board y se instancia el BoardView (canvas)
  */
-window.addEventListener("load", main);
+self.addEventListener("load", main);
 
 function main() {
     /**
      * Se define el tamaño del table
      */
     var board = new Board(800, 400);
+    var bar = new Bar(0, 100, 40, 100, board);
+    var bar = new Bar(760, 20, 40, 100, board);
     var canvas = document.getElementById("canvas");
-    var boardView = new BoardView();
+    var boardView = new BoardView(canvas, board);
+
+    boardView.draw();
 }
